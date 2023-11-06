d8ec31907ee39994d841dcd315d3588865110f70
Julian <julian@wheresnick.com.au>
2023-11-06 15:12:33 +1100

    added page routes and to retrieve any exisiting notes

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+const fs = require('fs');
+const path = require('path');
 app.listen(PORT, () => {
 });
 
@@ -10,3 +11,30 @@ app.listen(PORT, () => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+// serve the landing page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+  });
+
+  // serve the notes page
+  app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+  });
+
+  // API route to retrieve/get notes
+app.get('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, 'db/db.json'), 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ message: "Notes data can not be retrieved!" });
+      }
+      res.json(JSON.parse(data));
+    });
+  });
+
+  // Return index.html for GET requests that are not handled
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+  });
+  
